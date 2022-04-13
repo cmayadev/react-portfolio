11e426dbf5d8a8b23f3b37763061afa0b4bb868b
cmayadev <sefirot.maya@gmail.com>
2022-04-13 23:54:06 +0200

    change experience design

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,76 +10,73 @@ const Experience = () => {
                     <p>Algunos sitios donde he trabajado con algo relacionado al mundo del desarrollo.</p>
                 </div>
                 <div className="experience-list">
-                    <div className="media experience-item">
-                        <div className="media-left">
-                            <div>
+
+                    <div class="timeline">
+                        <div class="timeline-container">
+                            <div class="timeline-icon">
                                 <img src="logos/omatech.jpg" alt="Omatech" />
                             </div>
+                            <div class="timeline-body">
+                                <h4 class="timeline-title">Desarrollador Web</h4>
+                                <span>Omatech</span>
+                                <p class="timeline-subtitle">Desarrollador frontend y backend</p>
+                                <span class="timeline-lapse">Oct 2021 - Actualidad</span>
+                            </div>
                         </div>
-                        <div className="media-body">
-                            <h4>Desarrollador Web</h4>
-                            <span>Octubre, 2021 - Actualidad</span>
-                            <p>Desarrollador frontend y backend</p>
-                        </div>
-                    </div>
-                    <div className="media experience-item">
-                        <div className="media-left">
-                            <div>
+                        <div class="timeline-container">
+                            <div class="timeline-icon">
                                 <img src="logos/basetis.jpg" alt="BaseTIS" />
                             </div>
+                            <div class="timeline-body">
+                                <h4 class="timeline-title">Desarrollador Web</h4>
+                                <span>Basetis</span>
+                                <p class="timeline-subtitle">Desarrollador frontend, backend y maquetación web</p>
+                                <span class="timeline-lapse">Abril, 2021 - Octubre, 2021 (7 meses)</span>
+                            </div>
                         </div>
-                        <div className="media-body">
-                            <h4>Desarrollador Web</h4>
-                            <span>Abril, 2021 - Octubre, 2021</span>
-                            <p>Desarrollador frontend y backend y maquetación web</p>
-                        </div>
-                    </div>
-                    <div className="media experience-item">
-                        <div className="media-left">
-                            <div>
+                        <div class="timeline-container success">
+                            <div class="timeline-icon">
                                 <img src="logos/studiogenesis.jpg" alt="Studio Genesis" />
                             </div>
+                            <div class="timeline-body">
+                                <h4 class="timeline-title">Desarrollador web</h4>
+                                <span>StudioGenesis</span>
+                                <p class="timeline-subtitle">Desarrollador frontend y backend en diversos proyectos: landings, b2b, webs corporativas...</p>
+                                <span class="timeline-lapse">Marzo, 2020 - Marzo 2021 (1 año 1 mes)</span>
+                            </div>
                         </div>
-                        <div className="media-body">
-                            <h4>Desarrollador Web</h4>
-                            <span>Marzo, 2020 - Marzo 2021 (1 año y 1 mes)</span>
-                            <p>Desarrollador frontend y backend en diversos proyectos: landings, b2b, webs corporativas...</p>
-                        </div>
-                    </div>
-                    <div className="media experience-item">
-                        <div className="media-left">
-                            <div>
+                        <div class="timeline-container warning">
+                            <div class="timeline-icon">
                                 <img src="logos/catdiari.jpg" alt="Catalunya Diari" />
                             </div>
+                            <div class="timeline-body">
+                                <h4 class="timeline-title">Desarrollador Web</h4>
+                                <span>delCamp.cat</span>
+                                <p class="timeline-subtitle">Mantenimiento y desarrollo de diarios digitales y otras páginas web como horóscopos o blogs, así como mantenimiento y desarrollo de la API que nutría de datos éstas webs.</p>
+                                <span class="timeline-lapse">Julio, 2018 - Diciembre 2019 (1 año 6 meses)</span>
+                            </div>
                         </div>
-                        <div className="media-body">
-                            <h4>Desarrollador Web</h4>
-                            <span>Julio, 2018 - Diciembre 2019 (1 año y 6 meses)</span>
-                            <p>Mantenimiento y desarrollo de diarios digitales y otras páginas web como horóscopos o blogs, así como mantenimiento y desarrollo de la API que nutría de datos éstas webs.</p>
-                        </div>
-                    </div>
-                    <div className="media experience-item">
-                        <div className="media-left">
-                            <div>
+                        <div class="timeline-container">
+                            <div class="timeline-icon">
                                 <img src="logos/tecnocom.jpg" alt="Tecnocom" />
                             </div>
-                        </div>
-                        <div className="media-body">
-                            <h4>Programador</h4>
-                            <span>Julio, 2015 - Julio 2017 (2 años y 1 mes)</span>
-                            <p>Mantenimiento de Aplicaciones Estalvi Caixabank con lenguaje PL/I basado en COBOL.</p>
-                        </div>
-                    </div>
-                    <div className="media experience-item">
-                        <div className="media-left">
-                            <div>
-                                <img src="logos/pchouse.jpg" alt="PC House" />
+                            <div class="timeline-body">
+                                <h4 class="timeline-title">Programador</h4>
+                                <span>Tecnocom</span>
+                                <p class="timeline-subtitle">Mantenimiento de Aplicaciones de Caixabank con lenguaje PL/I basado en COBOL.</p>
+                                <span class="timeline-lapse">Julio, 2015 - Julio 2017 (2 años 1 mes)</span>
                             </div>
                         </div>
-                        <div className="media-body">
-                            <h4>Técnico y Diseñador Web</h4>
-                            <span>2006 - 2007 (1 año)</span>
-                            <p>Reparación y montaje de ordenadores, instalación de hardware/software, atención al cliente, venta de productos y diseño de páginas web.</p>
+                        <div class="timeline-container info">
+                            <div class="timeline-icon">
+                                <img src="logos/pchouse.jpg" alt="PC House" />
+                            </div>
+                            <div class="timeline-body">
+                                <h4 class="timeline-title">Técnico y Diseñador Web</h4>
+                                <span>PC House</span>
+                                <p class="timeline-subtitle">Reparación y montaje de ordenadores, instalación de hardware/software, atención al cliente, venta de productos y diseño de páginas web.</p>
+                                <span class="timeline-lapse">Setiembre, 2006 - Setiembre, 2007 (1 año)</span>
+                            </div>
                         </div>
                     </div>
                 </div>
