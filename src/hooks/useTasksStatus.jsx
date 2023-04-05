e7b9f07cdc15939fdd80a47af7be98996fe7e84e
Chris Maya <cmayadev@gmail.com>
2023-04-05 18:49:35 +0200

    update icons

--- src/hooks/useTasksStatus.jsx
+++ src/hooks/useTasksStatus.jsx
@@ -1,66 +1,63 @@
 import { useState } from "react";
 
-const useTasksStatus =  () => {
+const useTasksStatus = () => {
+  const initialTasks = [
+    {
+      id: 1,
+      name: "My Computer",
+      icon: "my-pc",
+      large: "my-pc",
+      status: "",
+      active: false,
+      selected: false,
+      desktop: true,
+    },
+    {
+      id: 2,
+      name: "My Documents",
+      icon: "documents",
+      large: "documents",
+      status: "",
+      active: false,
+      selected: false,
+      desktop: true,
+    },
+    {
+      id: 3,
+      name: "Internet Explorer",
+      icon: "ie-html",
+      large: "ie",
+      status: "open",
+      active: true,
+      selected: false,
+      desktop: true,
+    },
+    {
+      id: 4,
+      name: "Windows Update",
+      type: "update",
+      icon: "windows-update",
+      large: "windows-update",
+      status: "",
+      active: false,
+      selected: false,
+      desktop: true,
+    },
+    {
+      id: 5,
+      name: "Recycle Bin",
+      icon: "bin-full",
+      large: "bin-full",
+      status: "",
+      active: false,
+      selected: false,
+      desktop: true,
+    },
+  ];
 
-    const initialTasks = [
-        {
-            id: 1,
-            name: "My Computer",
-            icon: "my-pc",
-            large: "my-pc",
-            status: '',
-            active: false,
-            selected: false,
-            desktop: true
-        },
-        {
-            id: 2,
-            name: "My Documents",
-            icon: "documents",
-            large: "documents",
-            status: '',
-            active: false,
-            selected: false,
-            desktop: true
-        },
-        {
-            id: 3,
-            name: "Internet Explorer",
-            icon: "ie-html",
-            large: "ie",
-            status: 'open',
-            active: true,
-            selected: false,
-            desktop: true
-        },
-        {
-            id: 4,
-            name: "Recycle Bin",
-            icon: "bin-full",
-            large: "bin-full",
-            status: '',
-            active: false,
-            selected: false,
-            desktop: true
-        },
-        {
-            id: 5,
-            name: "Windows Update",
-            type: "update",
-            icon: "windows-update",
-            large: "windows-update",
-            status: '',
-            active: false,
-            selected: false,
-            desktop: true
-        },
+  const [tasks, setTasks] = useState(initialTasks);
 
-
-    ];
-
-    const [tasks, setTasks] = useState(initialTasks);
-
-    return [tasks, setTasks];
+  return [tasks, setTasks];
 };
 
 export default useTasksStatus;
