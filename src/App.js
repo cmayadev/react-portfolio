b94f60205ca831563b02d950d513afc7efc1f531
cmayadev <cmayadev@gmail.com>
2023-12-18 12:31:31 +0100

    fix warning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Desktop from "./components/Desktop";
 import Portfolio from "./components/Portfolio";
 import { useDisplay } from "./contexts/DisplayContext";
 import "./App.css";
-import Tracking from "./components/Tracker/Tracking";
 
 function App() {
   const { display } = useDisplay();
